a6de4c28288b1af3d900aa12a317ab467a574179
realnine <ljk0252g@gmail.com>
2021-11-01 18:02:45 +0900

    modified 211101

--- client.c
+++ client.c
@@ -17,16 +17,27 @@ void	send_sig(int pid, char *message)
 	int	bit;
 	int	len;
 
-	bit = 64;
+	bit = 64; 
+		// bit 값의 변화 64 => 32 => 16 => 8 => 4 => 2 => 1 => 0 ( >> 연산의 결과)
+		// 64 = 100 0000 (아스키코드는 7bit로 표현가능)
+		// 32 = 010 0000
 	len = ft_strlen(message);
-	while (len >= 0)
+	while (len >= 0) // message길이 x 7(bit값 변화 횟수) 만큼 반복
 	{
-		if (*message & bit)
-			kill(pid, SIGUSR2);
+		//만약 *message 가 'a'이면 아스키코드 97(110 0001)
+		if (*message & bit) // *message(문자 하나)와 bit를 and 연산
+			kill(pid, SIGUSR2); 
 		else
 			kill(pid, SIGUSR1);
+		// bit가 100 0000(64) 일때, 'a'(110 0001)와 연산결과 => 100 0000 이기 때문에
+		// if ()문은 true
+		// bit가 010 0000(32) 일때, 'a'(110 0001)와 연산결과 => 010 0000 이기 때문에
+		// if ()문은 true
+		// bit가 001 0000(16) 일때, 'a'(110 0001)와 연산결과 => 000 0000 이기 때문에
+		// if ()문은 false
+
 		bit >>= 1;
-		if (bit == 0)
+		if (bit == 0) // 문자하나의 정보를 다 보냈으면 다음 문자
 		{
 			len--;
 			message++;
--- server.c
+++ server.c
@@ -14,13 +14,13 @@
 
 void	sighandler(int num)
 {
-	static char	c = 0;
+	static char	c = 0; // 클라이언트로 부터 전달 받을 한 문자의 아스키 코드 값
 	static int	bit = 64;
 
-	if (num == SIGUSR2)
-		c += bit;
+	if (num == SIGUSR2) // 해당 위치의 비트값이 1일 때,
+		c += bit; 
 	bit >>= 1;
-	if (bit == 0)
+	if (bit == 0) // 한 문자의 모든 비트값을 확인했으면 출력
 	{
 		ft_putchar_fd(c, 1);
 		if (c == '\0')
@@ -33,7 +33,9 @@ void	sighandler(int num)
 
 int		main(void)
 {
-	signal(SIGUSR1, sighandler);
+	signal(SIGUSR1, sighandler); // 실행이 아니라 설정해놓는 것
+		// 현재 프로세스에 SIGUSR1 시그널이 발생하면 sighandler()함수 실행
+		// SIGUSR1값이 sighandler()함수의 인수로 들어감
 	signal(SIGUSR2, sighandler);
 	ft_putstr_fd("pid : ", 1);
 	ft_putnbr_fd(getpid(), 1);
